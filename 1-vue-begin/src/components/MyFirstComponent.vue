ffc8864284a6dbc0ec2f791b01f98d7e268a7938
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-05-15 11:04:43 +0200

    Added project

---
+++ 1-vue-begin/src/components/MyFirstComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import MySecondComponent from "./MySecondComponent.vue";
+
+const message = "Hello world!";
+</script>
+
+<template>
+  <MySecondComponent />
+  <h3 class="header">{{ message }}</h3>
+</template>
+
+<style scoped>
+.header {
+  color: blue;
+}
+</style>
