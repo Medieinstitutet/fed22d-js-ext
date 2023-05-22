06f2508096ccbbde7ef4f05aae4b9159719769ea
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-05-22 10:39:12 +0200

    Changed to v-model added onMounted

--- movie-search/src/components/MovieSearchApp.vue
+++ movie-search/src/components/MovieSearchApp.vue
@@ -1,33 +1,35 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import { IMovie } from "../models/IMovie";
 import axios from "axios";
 import { IOmdbResponse } from "../models/IOmdbResponse";
 
-const movies = ref<IMovie[]>(
-  JSON.parse(localStorage.getItem("movies") || "[]")
-);
+const movies = ref<IMovie[]>([]);
 
 const userInput = ref("");
 
-const handleInput = (e: Event) => {
-  const theInput = e.target as HTMLInputElement;
-  userInput.value = theInput.value;
-};
+onMounted(() => {
+  const searchText = localStorage.getItem("searchText") || "star";
+  axios
+    .get<IOmdbResponse>("http://omdbapi.com/?apikey=416ed51a&s=" + searchText)
+    .then((response) => {
+      movies.value = response.data.Search;
+    });
+});
 
 const handleSubmit = async () => {
   let response = await axios.get<IOmdbResponse>(
     "http://omdbapi.com/?apikey=416ed51a&s=" + userInput.value
   );
   movies.value = response.data.Search;
-  localStorage.setItem("movies", JSON.stringify(response.data.Search));
+  localStorage.setItem("searchText", userInput.value);
   userInput.value = "";
 };
 </script>
 
 <template>
   <form @submit.prevent="handleSubmit">
-    <input type="text" :value="userInput" @input="handleInput" />
+    <input type="text" v-model="userInput" />
     <button>Sök</button>
   </form>
 
