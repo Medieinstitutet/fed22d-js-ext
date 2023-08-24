315462694704d701c30b98fe7db797997b7449af
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-08-24 14:45:58 +0200

    Added config

--- cookie-publishing/src/Router.tsx
+++ cookie-publishing/src/Router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, createHashRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Home } from "./components/Home";
 import { Layout } from "./components/Layout";
 
-export const Router = createHashRouter([
+export const Router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
